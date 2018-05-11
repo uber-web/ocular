aabad54cd38e239ebec6dccccc6732758dcb2bb8
Jerome Cukier <jerome@uber.com>
2018-05-10 17:54:55 -0700

    rebased

--- website/src/mdRoutes.js
+++ website/src/mdRoutes.js
@@ -1,14 +1,25 @@
-import basics1Introduction from 'docs/basics/1-introduction.md'
-import basics2GettingStarted from 'docs/basics/2-getting-started.md'
+import basicsIntroduction from 'docs/basics/1-introduction.md'
+
+import basicsGettingStartedWithOcular from 'docs/basics/2-getting-started.md'
+
 import gettingStarted from 'docs/getting-started.md'
-import referenceBuildYourOwnComponents1PresentingBaseComponents from 'docs/reference/build-your-own-components/1-presenting-base-components.md'
-import referenceBuildYourOwnComponents2AppAndRelatedComponents1App from 'docs/reference/build-your-own-components/2-app-and-related-components/1-App.md'
-import referenceBuildYourOwnComponents2AppAndRelatedComponents2Header from 'docs/reference/build-your-own-components/2-app-and-related-components/2-Header.md'
-import referenceBuildYourOwnComponents2AppAndRelatedComponents3Toc from 'docs/reference/build-your-own-components/2-app-and-related-components/3-Toc.md'
-import referenceBuildYourOwnComponents3HomeAndRelatedComponents1Home from 'docs/reference/build-your-own-components/3-home-and-related-components/1-Home.md'
-import referenceBuildYourOwnComponents3HomeAndRelatedComponents2Hero from 'docs/reference/build-your-own-components/3-home-and-related-components/2-Hero.md'
-import referenceBuildYourOwnComponents3HomeAndRelatedComponents3Footer from 'docs/reference/build-your-own-components/3-home-and-related-components/3-Footer.md'
-import referenceConfigReference from 'docs/reference/config-reference.md'
+
+import referenceBuildYourOwnComponentsPresentingOcularSBaseComponents from 'docs/reference/build-your-own-components/1-presenting-base-components.md'
+
+import referenceBuildYourOwnComponents2AppAppJs from 'docs/reference/build-your-own-components/2-app/1-App.md'
+
+import referenceBuildYourOwnComponents2AppHeaderJs from 'docs/reference/build-your-own-components/2-app/2-Header.md'
+
+import referenceBuildYourOwnComponents2AppTocJs from 'docs/reference/build-your-own-components/2-app/3-Toc.md'
+
+import referenceBuildYourOwnComponents3HomeHomeJs from 'docs/reference/build-your-own-components/3-home/1-Home.md'
+
+import referenceBuildYourOwnComponents3HomeHeroJs from 'docs/reference/build-your-own-components/3-home/2-Hero.md'
+
+import referenceBuildYourOwnComponents3HomeFooterJs from 'docs/reference/build-your-own-components/3-home/3-Footer.md'
+
+import referenceConfigurationReference from 'docs/reference/config-reference.md'
+
 
 export default [{
   name: "Documentation",
@@ -16,16 +27,17 @@ export default [{
   data: [
     {
       name: "Basics",
+      path: "/docs/basics",
       children: [
         {
           fileLocation: "/src/docs/basics/1-introduction.md",
-          name: "1 Introduction",
-          markdown: basics1Introduction
+          name: "Introduction",
+          markdown: basicsIntroduction
         },
         {
           fileLocation: "/src/docs/basics/2-getting-started.md",
-          name: "2 Getting started",
-          markdown: basics2GettingStarted
+          name: "Getting started with ocular",
+          markdown: basicsGettingStartedWithOcular
         }
       ]
     },
@@ -36,52 +48,56 @@ export default [{
     },
     {
       name: "Reference",
+      path: "/docs/reference",
       children: [
         {
           name: "Build your own components",
+          path: "/docs/reference/build-your-own-components",
           children: [
             {
               fileLocation: "/src/docs/reference/build-your-own-components/1-presenting-base-components.md",
-              name: "1 Presenting base components",
-              markdown: referenceBuildYourOwnComponents1PresentingBaseComponents
+              name: "Presenting ocular's base components",
+              markdown: referenceBuildYourOwnComponentsPresentingOcularSBaseComponents
             },
             {
-              name: "2 App and related components",
+              name: "2 App",
+              path: "/docs/reference/build-your-own-components/2-app",
               children: [
                 {
-                  fileLocation: "/src/docs/reference/build-your-own-components/2-app-and-related-components/1-App.md",
-                  name: "1 App",
-                  markdown: referenceBuildYourOwnComponents2AppAndRelatedComponents1App
+                  fileLocation: "/src/docs/reference/build-your-own-components/2-app/1-App.md",
+                  name: "App js",
+                  markdown: referenceBuildYourOwnComponents2AppAppJs
                 },
                 {
-                  fileLocation: "/src/docs/reference/build-your-own-components/2-app-and-related-components/2-Header.md",
-                  name: "2 Header",
-                  markdown: referenceBuildYourOwnComponents2AppAndRelatedComponents2Header
+                  fileLocation: "/src/docs/reference/build-your-own-components/2-app/2-Header.md",
+                  name: "Header js",
+                  markdown: referenceBuildYourOwnComponents2AppHeaderJs
                 },
                 {
-                  fileLocation: "/src/docs/reference/build-your-own-components/2-app-and-related-components/3-Toc.md",
-                  name: "3 Toc",
-                  markdown: referenceBuildYourOwnComponents2AppAndRelatedComponents3Toc
+                  fileLocation: "/src/docs/reference/build-your-own-components/2-app/3-Toc.md",
+                  name: "Toc js",
+                  markdown: referenceBuildYourOwnComponents2AppTocJs
                 }
               ]
             },
             {
-              name: "3 Home and related components",
+              name: "3 Home",
+              path: "/docs/reference/build-your-own-components/3-home",
               children: [
                 {
-                  fileLocation: "/src/docs/reference/build-your-own-components/3-home-and-related-components/1-Home.md",
-                  name: "1 Home",
-                  markdown: referenceBuildYourOwnComponents3HomeAndRelatedComponents1Home
+                  fileLocation: "/src/docs/reference/build-your-own-components/3-home/1-Home.md",
+                  name: "Home js",
+                  markdown: referenceBuildYourOwnComponents3HomeHomeJs
                 },
                 {
-                  fileLocation: "/src/docs/reference/build-your-own-components/3-home-and-related-components/2-Hero.md",
-                  name: "2 Hero",
-                  markdown: referenceBuildYourOwnComponents3HomeAndRelatedComponents2Hero
+                  fileLocation: "/src/docs/reference/build-your-own-components/3-home/2-Hero.md",
+                  name: "Hero js",
+                  markdown: referenceBuildYourOwnComponents3HomeHeroJs
                 },
                 {
-                  fileLocation: "/src/docs/reference/build-your-own-components/3-home-and-related-components/3-Footer.md",
-                  name: "3 Footer",
-                  markdown: referenceBuildYourOwnComponents3HomeAndRelatedComponents3Footer
+                  fileLocation: "/src/docs/reference/build-your-own-components/3-home/3-Footer.md",
+                  name: "Footer js",
+                  markdown: referenceBuildYourOwnComponents3HomeFooterJs
                 }
               ]
             }
@@ -89,8 +105,8 @@ export default [{
         },
         {
           fileLocation: "/src/docs/reference/config-reference.md",
-          name: "Config reference",
-          markdown: referenceConfigReference
+          name: "Configuration reference",
+          markdown: referenceConfigurationReference
         }
       ]
     }
