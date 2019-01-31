dd361c546f6d14364026d06c6fd28d9b9dcd2fb7
Jerome Cukier <jerome@uber.com>
2019-01-31 15:35:10 -0800

    rewrite local links

--- modules/gatsby/src/gatsby-node/create-pages.js
+++ modules/gatsby/src/gatsby-node/create-pages.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const assert = require('assert');
 
-const {log, COLOR} = require('../utils/log');
+const { log, COLOR } = require('../utils/log');
 
 // PATHS TO REACT PAGES
 const INDEX_PAGE = path.resolve(__dirname, '../templates/index.jsx');
@@ -22,7 +22,7 @@ assert(INDEX_PAGE && DOC_PAGE && EXAMPLES_PAGE && EXAMPLE_PAGE);
 // by gatsby.
 // We use graphgl to query for nodes and iterate
 module.exports = function createPages({ graphql, actions }, pluginOptions) {
-  log.log({color: COLOR.CYAN}, 'generating pages')();
+  log.log({ color: COLOR.CYAN }, 'generating pages')();
 
   // TODO/ib - plugin options no longer provided when we are not a plugin
   // We seem to be getting site metadata instead?
@@ -44,7 +44,7 @@ module.exports = function createPages({ graphql, actions }, pluginOptions) {
   }
 
   return Promise.all([docPromise, examplesPromise]);
-}
+};
 
 // Create static pages
 // NOTE: gatsby does automatically build pages from **top level** `/pages`, folder
@@ -55,9 +55,8 @@ function createStaticPages({ graphql, actions }) {
 
   createPage({
     component: INDEX_PAGE,
-    path: '/',
+    path: '/'
   });
-
 }
 
 function createExamplePages({ graphql, actions }) {
@@ -76,8 +75,7 @@ function createExamplePages({ graphql, actions }) {
         }
       }
     }
-  `)
-  .then(result => {
+  `).then(result => {
     console.log(result);
 
     if (result.errors) {
@@ -86,7 +84,7 @@ function createExamplePages({ graphql, actions }) {
       throw new Error(result.errors);
     }
 
-    const {EXAMPLES} = result.data.site.siteMetadata.config;
+    const { EXAMPLES } = result.data.site.siteMetadata.config;
 
     // If the no examples marker, return without creating pages
     if (EXAMPLES.length === 0 || EXAMPLES[0].title === 'none') {
@@ -104,8 +102,10 @@ function createExamplePages({ graphql, actions }) {
     for (const example of EXAMPLES) {
       const exampleName = example.title;
 
-      log.log({color: COLOR.CYAN, priority: 1},
-        `Creating example page ${JSON.stringify(example)}`)();
+      log.log(
+        { color: COLOR.CYAN, priority: 1 },
+        `Creating example page ${JSON.stringify(example)}`
+      )();
 
       createPage({
         path: example.path,
@@ -141,15 +141,26 @@ function createDocPages({ graphql, actions }) {
             }
           }
         }
+        site {
+          siteMetadata {
+            config {
+              ROOT_FOLDER
+            }
+          }
+        }
       }
     `
-  )
-  .then(result => {
+  ).then(result => {
     if (result.errors) {
       /* eslint no-console: "off" */
       console.log(result.errors);
       throw new Error(result.errors);
     }
+    const rootFolder = result.data.site.siteMetadata.config.ROOT_FOLDER;
+    const pathToSlug = result.data.allMarkdownRemark.edges.map(({ node }) => ({
+      source: node.fileAbsolutePath,
+      target: node.fields.slug
+    }));
 
     const tagSet = new Set();
     const categorySet = new Set();
@@ -164,12 +175,57 @@ function createDocPages({ graphql, actions }) {
         categorySet.add(edge.node.frontmatter.category);
       }
 
+      const relativeLinks = pathToSlug.reduce((prev, { source, target }) => {
+        // what we are doing here: for each markdown file, we create a mapping of different ways to
+        // link to another markdown file that we will honor.
+
+        // let's suppose that we want to go from a file:
+        // - physical location: /docs/my-files/source.md, slug: /docs/chapter-1/source
+        // to this file:
+        // - phyiscal location: /docs/developer-guide/target.md, slug: /docs/advanced-usage/api-reference/target
+
+        // by default, '../../advanced-usage/api/reference/target' would work (target file slug, relative to original slug)
+        // '/docs/advanced-usage/api-reference/target' would also work (absolute target slug)
+        // however, on github, those links wouldn't work as there is no phyiscal file behind that link.
+        // in github however: '/docs/developer-guide/target.md' (file name relative to root) or
+        // '../developer-guide/target.md' (relative file name) would work. Those links wouldn't work on the gatsby rendered
+        // page however (until that).
+
+        // we are creating a mapping so that ANY OF THESE 4 SYNTAXES would be honored.
+        // So, authors can use links that refer to physical files, and gatsby will render a link that works - the same link
+        // can work on github and gatsby
+
+        // note that often, the physical location and the slug are the same!
+        // However there is no guarantee that this will be the case.
+
+        const relativeToCurrentFile = path.relative(
+          edge.node.fileAbsolutePath,
+          source
+        );
+        const relativeToRootFolder = path.relative(rootFolder, source);
+        const relativeToCurrentSlug = path.relative(
+          edge.node.fields.path,
+          target
+        );
+
+        const absoluteTarget = `/${target}`;
+
+        return {
+          ...prev,
+          [relativeToCurrentFile]: absoluteTarget,
+          [relativeToRootFolder]: absoluteTarget,
+          [relativeToCurrentSlug]: absoluteTarget,
+          [target]: absoluteTarget
+        };
+      }, {});
+
       // console.log('Creating doc page at', edge.node.fields.path);
 
       createPage({
         path: edge.node.fields.path,
         component: DOC_PAGE,
         context: {
+          relativeLinks,
           slug: edge.node.fields.path,
           toc: 'docs'
         }
