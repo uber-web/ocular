f7329d5b9e70c0b932bc54c1f6b8a67119b20314
Jerome Cukier <jerome@uber.com>
2019-01-31 18:02:32 -0800

    rewrite local links (#83)
    * rewrite local links
    
    * replace reduce by foreach

--- modules/gatsby/src/templates/doc-n.jsx
+++ modules/gatsby/src/templates/doc-n.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
 
-import { graphql } from 'gatsby'
+import { graphql } from 'gatsby';
 
 // Query for the markdown doc by slug
 // (Note: We could just search the allMarkdown from WebsiteConfig ourselves)
@@ -21,19 +20,37 @@ export const query = graphql`
   }
 `;
 
+function replaceLinks(props) {
+  const { html } = props.data.docBySlug;
+  const { relativeLinks } = props.pageContext;
+
+  return html.replace(/href="([^"]+)"/g, (link, href) => {
+    // don't rewrite external links, don't rewrite links to anchors
+    if (href.startsWith('http') || href.startsWith('#')) {
+      // TODO - we could style them differently though
+      return link;
+    }
+    const hrefWithoutLeadingSlash = href.startsWith('/') ? href.slice(1) : href;
+    // replace links to:
+    // - known physical files, either relative to this file or relative to root
+    // - known routes, either relative to the route of this page or to the home page
+    // by a link to their corresponding route, expresed relative to the home page
+    return `href="${relativeLinks[hrefWithoutLeadingSlash]}"`;
+  });
+}
+
 export default class DocTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { html: replaceLinks(props) };
+  }
+
   render() {
-    const {data, pathContext} = this.props;
-    const { slug } = pathContext
-    const docNode = data.docBySlug
-    const doc = docNode.frontmatter
-    // if (!doc.id) {
-    //   doc.id = slug
-    // }
+    const { html } = this.state;
     return (
       <div>
-        <div dangerouslySetInnerHTML={{ __html: docNode.html }} />
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
-    )
+    );
   }
 }
