f0753444a919047832d989044727e675c3890cb1
Xiaoji Chen <xiaoji@uber.com>
2019-02-01 14:15:45 -0800

    consolidate monorepo and non-monorepo scripts

--- modules/dev-tools/scripts/lint.sh
+++ modules/dev-tools/scripts/lint.sh
@@ -3,9 +3,13 @@
 
 set -e
 
-npx prettier-check "{src,examples,test}/**/*.js" || echo "Running prettier." && npx prettier --write "{src,examples,test}/**/*.js" --loglevel warn
-
-npx eslint src test examples
+if [ -d "modules" ]; then
+  npx prettier-check "{modules,examples,test}/**/*.js" || echo "Running prettier." && npx prettier --write "{modules,examples,test}/**/*.js" --loglevel warn
+  npx eslint modules test examples
+else
+  npx prettier-check "{src,examples,test}/**/*.js" || echo "Running prettier." && npx prettier --write "{src,examples,test}/**/*.js" --loglevel warn
+  npx eslint src test examples
+fi
 
 # check if yarn.lock contains private registry information
 !(grep -q unpm.u yarn.lock) && echo 'Lockfile valid.' || (echo 'Please rebuild yarn file using public npmrc' && false)
