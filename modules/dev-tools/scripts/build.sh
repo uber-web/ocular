d9e55a35cc8103ba6127a0a0a43e1b6e02ff9640
Xiaoji Chen <xiaoji@uber.com>
2019-02-01 14:24:11 -0800

    support monorepo in build script

--- modules/dev-tools/scripts/build.sh
+++ modules/dev-tools/scripts/build.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
-BABEL_ENV=es6 npx babel src --config-file ./babel.config.js --out-dir dist/es6 --source-maps --ignore 'node_modules/' &&
-BABEL_ENV=esm npx babel src --config-file ./babel.config.js --out-dir dist/esm --source-maps --ignore 'node_modules/' &&
-BABEL_ENV=es5 npx babel src --config-file ./babel.config.js --out-dir dist/es5 --source-maps --ignore 'node_modules/'
+
+set -e
+
+build_module() {
+  CONFIG=$1
+
+  BABEL_ENV=es6 npx babel src --config-file $CONFIG --out-dir dist/es6 --source-maps
+  BABEL_ENV=esm npx babel src --config-file $CONFIG --out-dir dist/esm --source-maps
+  BABEL_ENV=es5 npx babel src --config-file $CONFIG --out-dir dist/es5 --source-maps
+}
+
+if [ -d "modules" ]; then
+  # monorepo
+  cd modules
+  for D in *; do (
+    echo "\033[1mBuilding modules/$D\033[0m"
+    cd $D
+    build_module ../../babel.config.js
+    echo ""
+  ); done
+else
+  build_module ./babel.config.js
+fi
