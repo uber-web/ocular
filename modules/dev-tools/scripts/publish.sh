e69a3dea4f79fce8628471936054498e852373e0
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-02-01 14:17:26 -0800

    Expose bootstrap script; consolidate monorepo scripts (#85)

--- modules/dev-tools/scripts/publish.sh
+++ modules/dev-tools/scripts/publish.sh
@@ -8,14 +8,30 @@ BASEDIR=$(dirname "$0")
 # beta or prod
 MODE=$1
 
-case $MODE in
-  "beta")
-    npm run build && npm run test && npm run tester dist && npm publish --tag beta
-    break;;
+if [ -d "modules" ]; then
+  case $MODE in
+    "beta")
+      # npm-tag argument: npm publish --tag <beta>
+      # cd-version argument: increase <prerelease> version
+      lerna publish --npm-tag beta --cd-version prerelease
+      break;;
 
-  "prod")
-    npm run build && npm run test && npm run tester dist && npm publish
-    break;;
+    "prod")
+      lerna publish --cd-version minor
+      break;;
 
-  *) ;;
-esac
+    *) ;;
+  esac
+else
+  case $MODE in
+    "beta")
+      npm run build && npm run test && npm run tester dist && npm publish --tag beta
+      break;;
+
+    "prod")
+      npm run build && npm run test && npm run tester dist && npm publish
+      break;;
+
+    *) ;;
+  esac
+fi
