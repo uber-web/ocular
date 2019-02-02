929ed97ee44b72ecdda6f9ff0e1b429d79d386f2
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-02-01 16:55:46 -0800

    Support monorepo in build & clean scripts (#86)

--- modules/dev-tools/scripts/clean.sh
+++ modules/dev-tools/scripts/clean.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-rm -fr dist && mkdir -p dist/es5 dist/esm dist/es6
+
+set -e
+
+clean() {
+  rm -fr dist && mkdir -p dist/es5 dist/esm dist/es6
+}
+
+if [ -d "modules" ]; then
+  # Monorepo
+  cd modules
+
+  for D in *; do (
+    cd $D
+    clean
+  ); done
+else
+  clean
+fi
