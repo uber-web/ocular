8fa6d55ffd8d32072647a727cfe0d8f5e6f8290d
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-03-06 10:59:25 -0800

    add config system (#104)

---
+++ modules/dev-tools/config/ocular.config.js
@@ -0,0 +1,29 @@
+const {resolve} = require('path');
+
+const DEFAULT_CONFIG = {
+  babel: {
+    plugins: ['version-inline']
+  },
+
+  aliases: {},
+
+  entry: {
+    'test-node': 'test/node',
+    'test-browser': 'test/browser',
+    'bench-node': 'test/bench/node',
+    'bench-browser': 'test/bench/browser',
+  }
+};
+
+let userConfig;
+try {
+  userConfig = require(resolve('./ocular.config.js'));
+} catch (err) {
+  userConfig = {};
+}
+
+module.exports = {
+  babel: Object.assign(DEFAULT_CONFIG.babel, userConfig.aliases),
+  aliases: Object.assign(DEFAULT_CONFIG.aliases, userConfig.aliases),
+  entry: Object.assign(DEFAULT_CONFIG.entry, userConfig.entry)
+};
