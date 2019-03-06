8e444c55094ce1ca4f4e767a027438d0476486d0
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-03-06 09:26:19 -0800

    use prettier on markdowns (#102)

--- modules/dev-tools/scripts/lint.sh
+++ modules/dev-tools/scripts/lint.sh
@@ -3,13 +3,52 @@
 
 set -e
 
-if [ -d "modules" ]; then
-  npx prettier-check "{modules,examples,test}/**/*.js" || echo "Running prettier." && npx prettier --write "{modules,examples,test}/**/*.js" --loglevel warn
-  npx eslint modules test examples
-else
-  npx prettier-check "{src,examples,test}/**/*.js" || echo "Running prettier." && npx prettier --write "{src,examples,test}/**/*.js" --loglevel warn
-  npx eslint src test examples
+# Lint selected directories
+# lint.sh DIR1,DIR2
+DIRECTORIES=$1
+MODE=$2
+
+if [ -z "$1" ]; then
+  echo "Lint: must specify directories"
+  exit 1
 fi
+
+case $MODE in
+  "fast")
+    echo "Running prettier & eslint..."
+
+    # only check changed files
+    set +e
+    FILES=`git diff HEAD --name-only | grep .js$`
+    set -e
+
+    if [ ! -z "${FILES}" ]; then
+      for f in $FILES
+        do
+          if [ -e "${f}" ]; then
+            npx prettier --write $f --loglevel warn
+            npx eslint $f
+          fi
+      done
+    fi
+
+    # add changes to commit
+    git add .
+    break;;
+
+  *)
+    echo "Checking prettier code styles..."
+
+    DIR_PATTERN="{$DIRECTORIES}/**/*.{js,md}"
+    ROOT_PATTERN="*.md"
+    npx prettier-check  "$DIR_PATTERN" "$ROOT_PATTERN" \
+        || echo "Running prettier." \
+        && npx prettier --loglevel warn --write "$DIR_PATTERN" "$ROOT_PATTERN"
+
+    echo "Running eslint..."
+    npx eslint "{$DIRECTORIES}"
+    ;;
+  esac
 
 # check if yarn.lock contains private registry information
 !(grep -q unpm.u yarn.lock) && echo 'Lockfile valid.' || (echo 'Please rebuild yarn file using public npmrc' && false)
