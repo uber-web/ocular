f2e2b27470fdff4bc7934c257ca99c90a611d7e8
Jerome Cukier <jerome@uber.com>
2019-03-12 17:04:36 -0700

    101 create query layer (#119)
    * created a query-layer component that only contains static query info
    
    * also update ocular config template
    
    * changing query-layer to site-query

---
+++ modules/gatsby/src/components/layout/layout.jsx
@@ -0,0 +1,50 @@
+// This is the top-level "Layout" component that doesn't get unmounted between
+// page loads. This component is wrapped around the react component returned by
+// each page using the gatsby browser/SSR `wrapPage` callback.
+
+import React from 'react';
+import { lightThemePrimitives, createTheme } from '../styled/theme';
+import SiteQuery from '../query-layer';
+import TopLevelLayout from './top-level-layout';
+
+// Note: gatsby-plugin-sass will process these files automatically when it sees this import
+import '../../../styles/main.scss';
+
+// The Layout instance is shared between pages. It queries common, static data
+// and makes it available on React context
+export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.queryComplete = this.queryComplete.bind(this);
+  }
+
+  queryComplete(data) {
+    const { children } = this.props;
+    const { config } = data.site.siteMetadata;
+    const { tableOfContents, allMarkdown } = data;
+
+    // console.log('StaticQuery result', config, tableOfContents, allMarkdown);
+    const themeFromConfig = (
+      (config && config.THEME_OVERRIDES) ||
+      []
+    ).reduce((prev, curr) => ({ ...prev, [curr.key]: curr.value }), {});
+
+
+    const theme = createTheme({ ...lightThemePrimitives, ...themeFromConfig });
+    return (
+      <TopLevelLayout
+        {...this.props}
+        config={config}
+        tableOfContents={tableOfContents}
+        allMarkdown={allMarkdown}
+        theme={theme}
+      >
+        {children}
+      </TopLevelLayout>
+    );
+  }
+
+  render() {
+    return <SiteQuery onComplete={this.queryComplete} />;
+  }
+}
