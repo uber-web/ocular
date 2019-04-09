bcc80ca2b40dfb5be0d3c23d17b4754bb09874b6
jerome <jerome@uber.com>
2019-04-09 14:57:28 -0700

    disable passing theme through props to make builds pass

--- modules/gatsby/src/components/styled/index.js
+++ modules/gatsby/src/components/styled/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import styled from 'styled-components';
 import theme from './theme';
 /* eslint-disable import/prefer-default-export */
@@ -17,21 +18,21 @@ export const BodyContainerToC = styled.div(props => ({
   gridColumn: '2 / 3',
   gridRow: '2 / 3',
   width: '100%',
-  padding: props.theme.sizing.scale500,
-  [`@media screen and (max-width: ${props.theme.breakpoints.medium})`]: {
+  padding: theme.sizing.scale500,
+  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
     order: 2
   },
 
   '& > div': {
-    maxWidth: props.theme.breakpoints.large,
+    maxWidth: theme.breakpoints.large,
     margin: 'auto'
   },
   '& p': {
-    marginBottom: props.theme.sizing.scale500
+    marginBottom: theme.sizing.scale500
   },
 
   '& > h1': {
-    color: props.theme.colors.mono1000
+    color: theme.colors.mono1000
   }
 }));
 
@@ -52,7 +53,7 @@ export const HeaderContainer = styled.div(props => ({
   gridColumn: '1 / 3',
   gridRow: '1 / 2',
   zIndex: 2,
-  [`@media screen and (max-width: ${props.theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
     order: 1
   }
 }));
@@ -60,9 +61,9 @@ export const HeaderContainer = styled.div(props => ({
 export const ToCContainer = styled.div(props => ({
   gridColumn: '1 / 2',
   gridRow: '2 / 3',
-  background: props.theme.colors.mono200,
+  background: theme.colors.mono200,
   overflow: 'scroll',
-  [`@media screen and (max-width: ${props.theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
     order: 3,
     overflow: 'inherit'
   }
@@ -72,7 +73,7 @@ export const ToCContainer = styled.div(props => ({
 
 export const MainExample = styled.main(props => ({
   height: 'calc(100vh - 96px)',
-  [`@media screen and (max-width: ${props.theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
     marginTop: '64px'
   }
 }));
@@ -80,34 +81,34 @@ export const MainExample = styled.main(props => ({
 // examples
 
 export const MainExamples = styled.main(props => ({
-  background: props.theme.colors.mono100,
+  background: theme.colors.mono100,
   display: 'flex',
   flexWrap: 'wrap',
-  [`@media screen and (max-width: ${props.theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
     marginTop: '64px'
   }
 }));
 
 export const ExampleCard = styled.div(props => ({
-  border: props.theme.border300,
+  border: theme.border300,
   cursor: 'pointer',
-  margin: props.theme.sizing.scale400,
-  padding: `${props.theme.sizing.scale700} ${props.theme.sizing.scale600} ${
-    props.theme.sizing.scale700
-  } ${props.theme.sizing.scale600}`,
-  transition: `background ${props.theme.animation.timing400} border-color ${
-    props.theme.animation.timing400
+  margin: theme.sizing.scale400,
+  padding: `${theme.sizing.scale700} ${theme.sizing.scale600} ${
+    theme.sizing.scale700
+  } ${theme.sizing.scale600}`,
+  transition: `background ${theme.animation.timing400} border-color ${
+    theme.animation.timing400
   }`,
-  transitionTimingFunction: props.theme.animation.easeInOutCurve,
+  transitionTimingFunction: theme.animation.easeInOutCurve,
   '&:hover': {
-    background: props.theme.colors.mono200,
+    background: theme.colors.mono200,
     borderColor: 'transparent'
   }
 }));
 
 export const ExampleTitle = styled.div(props => ({
-  color: props.theme.colors.mono1000,
-  ...props.theme.typography.font,
+  color: theme.colors.mono1000,
+  ...theme.typography.font,
   marginBottom: 0,
   overflow: 'hidden',
   textOverflow: 'ellipsis',
@@ -119,9 +120,9 @@ export const ExampleTitle = styled.div(props => ({
 
 export const SearchContainer = styled.div(props => ({
   position: 'relative',
-  height: props.theme.sizing.scale1000,
+  height: theme.sizing.scale1000,
   marginBottom: '20px',
-  background: props.theme.colors.mono200
+  background: theme.colors.mono200
 }));
 
 export const IconContainer = styled.div(props => ({
@@ -129,8 +130,8 @@ export const IconContainer = styled.div(props => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  width: props.theme.sizing.scale1000,
-  height: props.theme.sizing.scale1000
+  width: theme.sizing.scale1000,
+  height: theme.sizing.scale1000
 }));
 
 export const InputSearch = styled.input`
