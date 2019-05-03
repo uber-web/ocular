162c4fece01a379d0bf37227e712a5a5452f0769
Ib Green <ibgreen@users.noreply.github.com>
2019-05-02 18:06:45 -0700

    Webpack config control and example layout (#153)

--- modules/gatsby/src/components/index.js
+++ modules/gatsby/src/components/index.js
@@ -1,6 +1,12 @@
 // example page components
-export {default as ExampleTableOfContents} from './layout/example-table-of-contents';
+export {default as TopLevelLayout} from './layout/top-level-layout';
+export {default as Layout} from './layout/layout';
 export {default as WithConfig} from './layout/website-config';
+export {default as TableOfContents} from './layout/table-of-contents';
+export {default as ExampleTableOfContents} from './layout/example-table-of-contents';
+export {default as Header} from './layout/header';
+export {default as Footer} from './layout/footer';
+
 export { MainExample } from './styled';
 
 export {default as Home} from './home';
