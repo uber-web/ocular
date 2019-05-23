ba1032c887d31d05837ab9acc2e60bd758d22c7f
Javid Hsueh <javid@uber.com>
2019-05-23 15:46:24 -0700

    Install ocular-dev-tools

---
+++ test/index.js
@@ -0,0 +1,6 @@
+// this makes Node.js understand import/export,
+// but otherwise does not transpile your code.
+require('reify');
+
+// start to import tests
+require('./modules/gatsby/test/index.js');
