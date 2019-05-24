ef5ec2dba68db095ed315ef813eeece46ecb7871
Javid Hsueh <javid@uber.com>
2019-05-23 17:19:57 -0700

    Delete yarn.lock, test/index.js

--- test/index.js
+++
@@ -1,6 +0,0 @@
-// this makes Node.js understand import/export,
-// but otherwise does not transpile your code.
-require('reify');
-
-// start to import tests
-require('./modules/gatsby/test/index.js');
