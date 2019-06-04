f537e7e5ba532e10cdb9f4e2a595f26cad1263d4
Javid Hsueh <javid@uber.com>
2019-06-04 15:50:53 -0700

    Rename post-install script, add warnings in the files will be overwritten

--- modules/gatsby/website-template/gatsby-browser.js
+++ modules/gatsby/website-template/gatsby-browser.js
@@ -1,1 +1,2 @@
+// WARNING: DO NOT MODIFY THIS FILE MANUALLY IT WILL BE OVERWRITTEN.
 module.exports = require('ocular-gatsby/gatsby-browser');
--- modules/gatsby/website-template/gatsby-config.js
+++ modules/gatsby/website-template/gatsby-config.js
@@ -1,3 +1,4 @@
+// WARNING: DO NOT MODIFY THIS FILE MANUALLY IT WILL BE OVERWRITTEN.
 const {getGatsbyConfig} = require('ocular-gatsby/api');
 
 const config = require('./ocular-config');
