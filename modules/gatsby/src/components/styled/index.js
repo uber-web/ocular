945055e417be7deafd1b7dadc17c476f6645fd9c
Jerome Cukier <jerome@uber.com>
2019-07-02 13:24:22 -0700

    create base web shim so we can keep on using styled component with an API that looks like styletron (#214)

--- modules/gatsby/src/components/styled/index.js
+++ modules/gatsby/src/components/styled/index.js
@@ -1,11 +1,49 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-unused-vars */
-import styled from 'styled-components';
+import React from 'react';
+import styledComponents from 'styled-components';
+
 import theme from './theme';
 /* eslint-disable import/prefer-default-export */
 
+// baseweb shim
+// this function replicates the baseweb API, but uses styledComponents under the hood.
+// when we'll be ready to use baseweb, we can simply import {styled} from 'baseui'
+// and remove this function as well as the styled-components import
+
+function styleHyphenFormat(propertyName) {
+  function upperToHyphenLower(match, offset, string) {
+    return (offset > 0 ? '-' : '') + match.toLowerCase();
+  }
+  return propertyName.replace(/[A-Z]/g, upperToHyphenLower);
+}
+
+const styleObjectToString = (obj, depth = 0) => {
+  return Object.entries(obj).reduce((result, [key, value], index) => {
+    const recursiveValue =
+      typeof value === 'object'
+        ? `{\n${styleObjectToString(value, depth + 1)}\n}`
+        : `${value};`;
+    const optionalNewLine = index ? '\n' : '';
+    const indentation = ' '.repeat(depth * 2);
+    const hyphenatedKey = styleHyphenFormat(key);
+    const formattedKey = hyphenatedKey.includes(' ') ? `"${hyphenatedKey}"` : hyphenatedKey;
+    const optionalColon = typeof value === 'object' ? ' ' : ': '
+    return `${result}${optionalNewLine}${indentation}${hyphenatedKey}${optionalColon}${recursiveValue}`;
+  }, '');
+};
+
+function styled(element, stylingFunction) {
+  const styledComponentLiteral = styleObjectToString(stylingFunction({$theme: theme}));
+  return styledComponents[element]`${styledComponentLiteral}`;
+}
+
+
+
 // top-level layoout
 
-export const BodyContainerFull = styled.div(props => ({
+export const BodyContainerFull = styled('div', ({$theme, ...props}) => ({
   margin: '0 auto',
 
   '.contributors': {
@@ -14,56 +52,57 @@ export const BodyContainerFull = styled.div(props => ({
   }
 }));
 
-export const BodyContainerToC = styled.div(props => ({
+
+export const BodyContainerToC = styled('div', ({$theme, ...props}) => ({
   gridColumn: '2 / 3',
   gridRow: '2 / 3',
   width: '100%',
-  padding: theme.sizing.scale500,
-  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
+  padding: $theme.sizing.scale500,
+  [`@media screen and (max-width: ${$theme.breakpoints.medium})`]: {
     order: 2
   },
 
   '& > div': {
-    maxWidth: theme.breakpoints.large,
+    maxWidth: $theme.breakpoints.large,
     margin: 'auto'
   },
   '& p': {
-    marginBottom: theme.sizing.scale500
+    marginBottom: $theme.sizing.scale500
   },
 
   '& > h1': {
-    color: theme.colors.mono1000
+    color: $theme.colors.mono1000
   }
 }));
 
-export const BodyGrid = styled.div(props => ({
+export const BodyGrid = styled('div', ({$theme, ...props}) => ({
   height: '100vh',
   display: 'grid',
   gridTemplateRows: '64px 1fr',
   gridTemplateColumns: '300px 1fr',
-  maxWidth: `${theme.breakpoints.large}px`,
-  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
+  maxWidth: `${$theme.breakpoints.large}px`,
+  [`@media screen and (max-width: ${$theme.breakpoints.medium})`]: {
     display: 'flex',
     flexDirection: 'column',
     height: 'inherit'
   }
 }));
 
-export const HeaderContainer = styled.div(props => ({
+export const HeaderContainer = styled('div', ({$theme, ...props}) => ({
   gridColumn: '1 / 3',
   gridRow: '1 / 2',
   zIndex: 2,
-  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${$theme.breakpoints.medium})`]: {
     order: 1
   }
 }));
 
-export const ToCContainer = styled.div(props => ({
+export const ToCContainer = styled('div', ({$theme, ...props}) => ({
   gridColumn: '1 / 2',
   gridRow: '2 / 3',
-  background: theme.colors.mono200,
+  background: $theme.colors.mono200,
   overflow: 'scroll',
-  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${$theme.breakpoints.medium})`]: {
     order: 3,
     overflow: 'inherit'
   }
@@ -71,44 +110,44 @@ export const ToCContainer = styled.div(props => ({
 
 // example
 
-export const MainExample = styled.main(props => ({
+export const MainExample = styled('main', ({$theme, ...props}) => ({
   height: 'calc(100vh - 96px)',
-  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${$theme.breakpoints.medium})`]: {
     marginTop: '64px'
   }
 }));
 
 // examples
 
-export const MainExamples = styled.main(props => ({
-  background: theme.colors.mono100,
+export const MainExamples = styled('main', ({$theme, ...props}) => ({
+  background: $theme.colors.mono100,
   display: 'flex',
   flexWrap: 'wrap',
-  [`@media screen and (max-width: ${theme.breakpoints.medium})`]: {
+  [`@media screen and (max-width: ${$theme.breakpoints.medium})`]: {
     marginTop: '64px'
   }
 }));
 
-export const ExampleCard = styled.div(props => ({
-  border: theme.border300,
+export const ExampleCard = styled('div', ({$theme, ...props}) => ({
+  border: $theme.borders.border300,
   cursor: 'pointer',
-  margin: theme.sizing.scale400,
+  margin: $theme.sizing.scale400,
   padding: `${theme.sizing.scale700} ${theme.sizing.scale600} ${
-    theme.sizing.scale700
-  } ${theme.sizing.scale600}`,
-  transition: `background ${theme.animation.timing400} border-color ${
-    theme.animation.timing400
+    $theme.sizing.scale700
+  } ${$theme.sizing.scale600}`,
+  transition: `background ${$theme.animation.timing400} border-color ${
+    $theme.animation.timing400
   }`,
-  transitionTimingFunction: theme.animation.easeInOutCurve,
+  transitionTimingFunction: $theme.animation.easeInOutCurve,
   '&:hover': {
-    background: theme.colors.mono200,
+    background: $theme.colors.mono200,
     borderColor: 'transparent'
   }
 }));
 
-export const ExampleTitle = styled.div(props => ({
-  color: theme.colors.mono1000,
-  ...theme.typography.font,
+export const ExampleTitle = styled('div', ({$theme, ...props}) => ({
+  color: $theme.colors.mono1000,
+  ...$theme.typography.font200,
   marginBottom: 0,
   overflow: 'hidden',
   textOverflow: 'ellipsis',
@@ -118,38 +157,38 @@ export const ExampleTitle = styled.div(props => ({
 
 // search
 
-export const SearchContainer = styled.div(props => ({
+export const SearchContainer = styled('div', ({$theme, ...props}) => ({
   position: 'relative',
-  height: theme.sizing.scale1000,
+  height: $theme.sizing.scale1000,
   marginBottom: '20px',
-  background: theme.colors.mono200
+  background: $theme.colors.mono200
 }));
 
-export const IconContainer = styled.div(props => ({
+export const IconContainer = styled('div', ({$theme, ...props}) => ({
   position: 'absolute',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  width: theme.sizing.scale1000,
-  height: theme.sizing.scale1000
+  width: $theme.sizing.scale1000,
+  height: $theme.sizing.scale1000
 }));
 
-export const InputSearch = styled.input`
-  box-shadow: rgba(0, 0, 0, 0) 0px 2px 6px;
-  border: 1px solid transparent;
-  transition: 0.3s;
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 20px;
-  padding: 10px 10px 10px 40px;
-  :focus {
-    box-shadow: rgba(39, 110, 241, 0.32) 0px 2px 6px;
-    border-color: rgb(39, 110, 241);
-    outline: none;
+export const InputSearch = styled('input', ({$theme, ...props}) => ({
+  boxShadow: `rgba(0, 0, 0, 0) 0px 2px 6px`,
+  border: `1px solid transparent`,
+  transition: `0.3s`,
+  fontSize: `14px`,
+  fontWeight: 500,
+  lineHeight: `20px`,
+  padding: `10px 10px 10px 40px`,
+  ':focus': {
+    boxShadow: `rgba(39, 110, 241, 0.32) 0px 2px 6px`,
+    borderColor: `rgb(39, 110, 241)`,
+    outline: 'none'
   }
-`;
+}));
 
-export const MainSearch = styled.main`
-  max-width: 600px;
-  margin: 104px auto 0px;
-`;
+export const MainSearch = styled('main', ({$theme, ...props}) => ({
+  maxWidth: '600px',
+  margin: '104px auto 0px'
+}));
