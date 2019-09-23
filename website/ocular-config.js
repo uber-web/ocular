2c535a29b7f90e48ce0d71189d7e2f73e10825d7
Jerome Cukier <jerome@uber.com>
2019-09-23 16:46:31 -0700

    Documentation update (#253)
    * wip
    
    * wip
    
    * documentation update

--- website/ocular-config.js
+++ website/ocular-config.js
@@ -1,60 +1,42 @@
-const resolve = require('path').resolve;
 
+// default location for table of contents
 const DOCS = require('../docs/table-of-contents.json');
-const DEPENDENCIES = require('./package.json').dependencies;
-// eslint-disable-next-line import/no-extraneous-dependencies
-const ALIASES = {};
-// require('ocular-dev-tools/config/ocular.config')({
-//   root: resolve(__dirname, '..')
-// }).aliases;
-
-// When duplicating example dependencies in website, autogenerate
-// aliases to ensure the website version is picked up
-// NOTE: module dependencies are automatically injected
-// TODO - should this be automatically done by ocular-gatsby?
-const dependencyAliases = {};
-for (const dependency in DEPENDENCIES) {
-  dependencyAliases[dependency] = `${__dirname}/node_modules/${dependency}`;
-}
 
 module.exports = {
+  // Adjusts amount of debug information from ocular-gatsby
   logLevel: 4,
 
   DOC_FOLDER: `${__dirname}/../docs/`,
   ROOT_FOLDER: `${__dirname}/../`,
   DIR_NAME: `${__dirname}`,
 
+  EXAMPLES: [
+    // {
+    //   title: 'my example',
+    //   path: 'examples/my-example/',
+    //   image: 'images/my-example.jpg',
+    //   componentUrl: '../examples/app.js'
+    // }
+  ],
+  // your table of contents go there
   DOCS,
 
-  // TODO/ib - from ocular, deduplicate with above settings
-  PROJECT_TYPE: 'github',
+  THEME_OVERRIDES: [
+    //  {key: 'primaryFontFamily', value: 'serif'}
+  ],
 
-  PROJECT_NAME: 'ocular',
+  PROJECT_TYPE: 'github',
+  PROJECT_NAME: 'Ocular',
   PROJECT_ORG: 'uber-web',
   PROJECT_URL: 'https://github.com/uber-web/ocular',
-  PROJECT_DESC: 'A documentation website for Ocular, made with Ocular',
-  PATH_PREFIX: '/ocular',
+  PROJECT_DESC: 'Uber\'s open source documentation system',
+  PATH_PREFIX: '',
 
   FOOTER_LOGO: '',
 
-  PROJECTS: [],
-
   HOME_PATH: '/',
-
-  HOME_HEADING: 'A documentation website for Ocular, made with Ocular',
-
+  HOME_HEADING: 'Uber\'s open source documentation system',
   HOME_RIGHT: null,
-
-  GA_TRACKING: null,
-
-  // For showing star counts and contributors.
-  // Should be like btoa('YourUsername:YourKey') and should be readonly.
-  GITHUB_KEY: null,
-
-  LINK_TO_GET_STARTED: '/docs/get-started',
-
-  ADDITIONAL_LINKS: [],
-
   HOME_BULLETS: [
     {
       text: 'Designed for React',
@@ -68,40 +50,15 @@ module.exports = {
   ],
 
   PROJECTS: [
-    {
-      name: 'deck.gl',
-      url: 'https://deck.gl'
-    },
-    {
-      name: 'luma.gl',
-      url: 'https://luma.gl'
-    },
-    {
-      name: 'react-map-gl',
-      url: 'https://uber.github.io/react-map-gl'
-    },
-    {
-      name: 'react-vis',
-      url: 'https://uber.github.io/react-vis'
-    },
-    {
-      name: 'vis.gl',
-      url: 'https://vis.gl'
-    }
+    // {name: 'Project name', url: 'http://project.url'},
+  ],
+  ADDITIONAL_LINKS: [
+    // {name: 'link label', href: 'http://link.url'}
   ],
 
-  // INDEX_PAGE_URL: resolve(__dirname, './templates/index.jsx'),
+  GA_TRACKING: null,
 
-  EXAMPLES: [
-    {
-      title: 'Minimal Example',
-      image: 'images/icon-high-precision.svg',
-      componentUrl: resolve(__dirname, '../examples/minimal/app.js'),
-      path: 'examples/minimal'
-    }
-  ],
-
-  // Avoids duplicate conflicting inputs when importing from examples folders
-  // Ocular adds this to gatsby's webpack config
-  WEBPACK_ALIAS: Object.assign({}, ALIASES, dependencyAliases)
+  // For showing star counts and contributors.
+  // Should be like btoa('YourUsername:YourKey') and should be readonly.
+  GITHUB_KEY: null
 };
