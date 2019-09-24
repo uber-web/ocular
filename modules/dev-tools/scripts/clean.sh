784e79479a51cf7e6bde812d4dcff1f87dfd3567
Hong Wang <hongw@uber.com>
2019-09-24 13:00:10 -0400

    tweak build to accept options to build selected targets. (#251)
    tweak ocular-build to accept options to build selected targets. Addressing this issue https://github.com/uber/manifold/issues/31.
    
    **ocular-build**
    
    Usage:
    
    ```
    ocular-build [--dist es5|es6|esm,...] [module,...]
    ```
    
    Example:
    
    ```
    ocular-build --dist es6,esm
    ocular-build --dist esm
    ocular-build --dist es6,esm module1
    ocular-build module1
    ```
    
    New Behaviors:
    1. If there's only one option after `--dist`, then the bundle will be built into `dist/` (without `esm/` etc .)
    2. If there are unrecognized parameter after the `--dist` option, ocular will give an error message and exit.
    3. The pattern with `\-\-[a-zA-Z]+` after `ocular-build` is now reserved for options, in the other words, there can't be a module named as these, e.g. `--m` as module...
    
    **ocular-clean**
    
    Usage
    
    ```
    ocular-clean [all]
    ```
    
    New Behavior:
    1. If the `all' option is used, clean the entire dist/ without making empty es5/ es6/ esm/

--- modules/dev-tools/scripts/clean.sh
+++ modules/dev-tools/scripts/clean.sh
@@ -3,7 +3,14 @@
 set -e
 
 clean() {
-  rm -fr dist && mkdir -p dist/es5 dist/esm dist/es6
+  if [ -z "$1" ]; then
+    rm -fr dist && mkdir -p dist/es5 dist/esm dist/es6
+  elif [ "$1" = "all" ]; then
+    rm -fr dist
+  else
+    echo -e "\033[91mUnknown option $1. ocular-clean [all]\033[0m"
+    exit 1
+  fi
 }
 
 if [ -d "modules" ]; then
@@ -12,8 +19,8 @@ if [ -d "modules" ]; then
 
   for D in *; do (
     cd $D
-    clean
+    clean $1
   ); done
 else
-  clean
+  clean $1
 fi
