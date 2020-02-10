109a2c1107c2fe5d7c275cea56fbbd29d08f6988
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2020-02-09 17:18:16 -0800

    Fix relative path in stylesheets (#297)

--- modules/gatsby-theme-ocular/src/react/templates/examples.jsx
+++ modules/gatsby-theme-ocular/src/react/templates/examples.jsx
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import {relative} from 'path';
+import {withPrefix} from "gatsby"
 
 import {MainExamples, ExampleCard, ExampleTitle} from '../styled/example';
 
 export default class Examples extends Component {
   render() {
     const {
-      path,
       pageContext: {examples}
     } = this.props;
     return (
@@ -14,7 +13,7 @@ export default class Examples extends Component {
         {examples.map(exampleData => (
           <ExampleCard key={exampleData.title} to={`/${exampleData.path}`}>
             {exampleData.imageSrc ? (
-              <img width="100%" src={relative(path, exampleData.imageSrc)} alt={exampleData.title} />
+              <img width="100%" src={withPrefix(exampleData.imageSrc)} alt={exampleData.title} />
             ) : null}
             <ExampleTitle>{exampleData.title}</ExampleTitle>
           </ExampleCard>
