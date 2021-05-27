adf25cdc0c7ba03d26a74969775b5035446d1d60
Ib Green <ib@unfolded.ai>
2021-05-27 07:53:38 -0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = (env = {}) => {
   const config = getWebpackConfig(env);
 
   // Make any changes to default config here
+  // https://github.com/prettier/prettier/issues/4879
+  console.error(env);
+  delete config.node.fs;
 
   // Uncomment to debug config
   // console.error(JSON.stringify(config, null, 2));
