24e026e98090bd3fc7e68017933f4988fe4b684b
Nick Rabinowitz <public@nickrabinowitz.com>
2022-11-18 14:38:18 -0800

    fix: Fix error codes returned by ocular-test, ocular-lint, etc (#7)
    * Fix error codes returned by ocular-test, ocular-lint, etc
    
    * Drop node 12 from test matrix

--- modules/dev-tools/scripts/test.js
+++ modules/dev-tools/scripts/test.js
@@ -3,5 +3,7 @@
 const shell = require('shelljs');
 const path = require('path');
 
-// Runs the bash script and forward the arguments
-shell.exec(`${path.resolve(__dirname, './test.sh')} ${process.argv.slice(2).join(' ')}`);
+// Runs the bash script and forward the arguments, exiting with the same code
+shell.exit(
+  shell.exec(`${path.resolve(__dirname, './test.sh')} ${process.argv.slice(2).join(' ')}`).code
+);
