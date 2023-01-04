0970cb8d7a226f4d34ea4684d522a71292571458
Xiaoji Chen <cxiaoji@gmail.com>
2023-01-04 11:45:38 -0800

    Remove shelljs

--- modules/dev-tools/scripts/lint.js
+++ modules/dev-tools/scripts/lint.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import shell from 'shelljs';
+import {execShellCommand} from '../src/utils/shell.js';
 import {join} from 'path';
 
 const scriptDir = new URL(import.meta.url).pathname;
 // Runs the bash script and forward the arguments, exiting with the same code
-shell.exit(shell.exec(`${join(scriptDir, '../lint.sh')} ${process.argv.slice(2).join(' ')}`).code);
+execShellCommand(join(scriptDir, '../lint.sh'), process.argv.slice(2));
