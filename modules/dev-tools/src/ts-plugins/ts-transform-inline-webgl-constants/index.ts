45773e3e39dae19569573a0e473fba4ab4ce4870
Xiaoji Chen <cxiaoji@gmail.com>
2024-02-28 23:57:04 -0800

    Add ts-transform-inline-webgl-constants

---
+++ modules/dev-tools/src/ts-plugins/ts-transform-inline-webgl-constants/index.ts
@@ -0,0 +1,45 @@
+/**
+ * TypeScript transform to replaces `gl.<constant>` or `GL.<constant>` references with 
+ * the corresponding WebGL constant value. Requires `@luma.gl/constants` as peer dependency.
+ * Usage with ts-patch:
+   {
+      "plugins": [
+        {
+          "transform": "ocular-dev-tools/ts-transform-inline-webgl-constants"
+        }
+      ]
+   }
+ */
+import type {Program, TransformationContext, SourceFile, Node, StringLiteral} from 'typescript';
+import type {TransformerExtras, PluginConfig} from 'ts-patch';
+import {GL} from '@luma.gl/constants';
+
+export default function (program: Program, pluginConfig: PluginConfig, {ts}: TransformerExtras) {
+  return (ctx: TransformationContext) => {
+    const {factory} = ctx;
+
+    function filterLeftIdentifier(node: Node): boolean {
+      const left = node.getChildAt(0);
+      return ts.isIdentifier(left) && (left.text === 'GL' || left.text === 'gl');
+    }
+
+    return (sourceFile: SourceFile) => {
+      function visit(node: Node): Node {
+        if (ts.isPropertyAccessExpression(node) && filterLeftIdentifier(node)) {
+          const key = node.getChildAt(2);
+          if (ts.isIdentifier(key) && key.text in GL) {
+            return factory.createNumericLiteral(GL[key.text]);
+          }
+        }
+        if (ts.isElementAccessExpression(node) && filterLeftIdentifier(node)) {
+          const key = node.getChildAt(2);
+          if (ts.isStringLiteral(key) && key.text in GL) {
+            return factory.createNumericLiteral(GL[key.text]);
+          }
+        }
+        return ts.visitEachChild(node, visit, ctx);
+      }
+      return ts.visitNode(sourceFile, visit);
+    };
+  };
+}
--- modules/dev-tools/test/index.ts
+++ modules/dev-tools/test/index.ts
@@ -4,3 +4,4 @@ import './lib/configuration.spec';
 import './ts-plugins/ts-transform-version-inline.spec';
 import './ts-plugins/ts-transform-append-extension.spec';
 import './ts-plugins/ts-transform-remove-glsl-comments/index.spec';
+import './ts-plugins/ts-transform-inline-webgl-constants.spec';
